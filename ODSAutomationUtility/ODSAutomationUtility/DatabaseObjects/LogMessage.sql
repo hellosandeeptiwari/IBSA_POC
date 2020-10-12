89bc1c0383bc234b7fed67174fe2d4cba4c00bac
Sundeep Reddy Lingala <sundeepl@cnxsi.com>
2020-10-12 08:27:29 +0530

    C3-9456: Increased the length of Comments and ProcedureName columns.

--- ODSAutomationUtility/ODSAutomationUtility/DatabaseObjects/LogMessage.sql
+++ ODSAutomationUtility/ODSAutomationUtility/DatabaseObjects/LogMessage.sql
@@ -2,8 +2,7 @@
 IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'LogMessage')
     CREATE TABLE [dbo].[LogMessage] (
         [LogId]         INT             IDENTITY (1, 1) NOT NULL,
-        [ProcedureName] NVARCHAR (64)   NOT NULL,
-        [Comments]      NVARCHAR (1024) NULL,
-        [LogUTCDate]    DATETIME        NOT NULL,
-        CONSTRAINT [PK_LogMessage] PRIMARY KEY CLUSTERED ([LogId] ASC)
+        [ProcedureName] VARCHAR (128)   NOT NULL,
+        [Comments]      NVARCHAR (4000) NULL,
+        [LogUTCDate]    DATETIME        NOT NULL
     );
